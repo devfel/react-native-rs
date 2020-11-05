c14ec5f22e15f907d2bb8f1f8e3f9e0e95610872
Felizardo <devfelizardo@gmail.com>
2020-11-05 04:31:13 -0300

    testing reactJS and react native differences in styles and components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar backgroundColor="#00F" barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World Feliz</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#FF0',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+});
